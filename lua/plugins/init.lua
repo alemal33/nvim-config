cd684d8a9ba0d5c2ff0960ba43d35be91fda7869
Alexandr Malykh <sanko33mavr@ya.ru>
2022-11-14 23:25:53 +0400

    refactor

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,14 +1,53 @@
-local function include(name)
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+		vim.cmd 'packadd packer.nvim'
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- Get config file
+local function get(name)
 	require('plugins.' .. name)
 end
 
-include('packer')
+return require('packer').startup(function(use)
 
-include('lastplace')
-include('onedark')
-include('comment')
-include('telescope')
-include('gitsigns')
-include('mason')
-include('lspconfig')
-include('lualine')
+	-- Package manager
+	use 'wbthomason/packer.nvim'
+	use { 'williamboman/mason.nvim', config = get('mason') }
+
+	-- Telescope
+	use {
+		'nvim-telescope/telescope.nvim', branch = '0.1.x',
+		requires = { 'nvim-lua/plenary.nvim' },
+		config = get('telescope')
+	}
+	use 'natecraddock/telescope-zf-native.nvim'
+
+	-- Lsp
+	use { 'williamboman/mason-lspconfig.nvim', config = get('lspconfig') }
+	use 'neovim/nvim-lspconfig'
+
+	-- UI
+	use 'kyazdani42/nvim-web-devicons'
+	use { 'nvim-lualine/lualine.nvim', config = get('lualine') }
+	use { 'navarasu/onedark.nvim', config = get('onedark') }
+
+	use { 'ethanholz/nvim-lastplace', config = require('nvim-lastplace').setup() }
+	use { 'numToStr/Comment.nvim', config = get('comment') }
+
+	use 'nvim-treesitter/nvim-treesitter'
+	use 'nvim-lua/plenary.nvim'
+
+	use { 'lewis6991/gitsigns.nvim', config = get('gitsigns') }
+
+	if packer_bootstrap then
+		require('packer').sync()
+	end
+end)
--- lua/plugins/lastplace.lua
+++
@@ -1,3 +0,0 @@
-require('nvim-lastplace').setup {
-
-}
