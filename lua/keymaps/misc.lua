30365c762e5907a88a3acff6e4b8b81de9baf655
alemal33 <sanko33mavr@ya.ru>
2022-11-21 14:49:57 +0400

    (keymaps) telescope misc

--- lua/keymaps/misc.lua
+++ lua/keymaps/misc.lua
@@ -4,3 +4,10 @@ vim.keymap.set('i', '<c-s>', '<c-o>:w<CR>')
 
 -- change directory
 vim.keymap.set('n', '<LEADER>cd', function() vim.cmd(':cd %:p:h'); print('cd ' .. vim.fn.getcwd()) end)
+
+-- Telescope
+NTelescopeMap('T', 'builtin')
+NTelescopeMap(' ', 'resume')
+NTelescopeMap('H', 'help_tags')
+NTelescopeMap('m', 'marks')
+NTelescopeMap('r', 'registers')
