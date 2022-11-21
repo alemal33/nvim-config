20b03c3b5bc2e11f04a4a196a37d2c2efc565d67
alemal33 <sanko33mavr@ya.ru>
2022-11-21 13:57:27 +0400

    (keymaps) save

--- lua/keymaps/misc.lua
+++ lua/keymaps/misc.lua
@@ -1,2 +1,6 @@
+-- [CTRL + S] :write
+vim.keymap.set('n', '<c-s>', ':w<CR>')
+vim.keymap.set('i', '<c-s>', '<c-o>:w<CR>')
+
 -- change directory
 vim.keymap.set('n', '<LEADER>cd', function() vim.cmd(':cd %:p:h'); print('cd ' .. vim.fn.getcwd()) end)
